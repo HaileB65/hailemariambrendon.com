b76f9e70964779d95b3108c324a86ecc456b6d42
Hailemariam Brendon <hailemariam.brendon@gmail.com>
2025-08-13 21:37:14 -0500

    First push from of hailemariambrendon.com from local git

---
+++ hailemariambrendon/src/main/java/com/example/hailemariambrendon/HailemariambrendonApplication.java
@@ -0,0 +1,13 @@
+package com.example.hailemariambrendon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HailemariambrendonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HailemariambrendonApplication.class, args);
+	}
+
+}
